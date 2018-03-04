f64ddab96426fb67955c9ce047a4763fdeea4060
Yuri <hamta@yandex.ru>
2018-03-04 12:20:16 +0300

    All done

---
+++ A/preparing.py
@@ -0,0 +1,2 @@
+header = open("index.h", 'w')
+header.write("//This header created by preparing.py.\nconst int varFromDirA = 42;")
---
+++ B/lib.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void BlibF(){
+	std::cout<<"Function from libB"<<std::endl;
+}
